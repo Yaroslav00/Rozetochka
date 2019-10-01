5dabacac9df3445c928fe577d44fab13da0c005c
TarasBat <taras,bat2000@gmail.com>
2019-10-01 22:31:55 +0300

    Implemented

---
+++ Rozetochka/DataAccess/Order.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public DateTime Data { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool PaymentStatus { get; set; }
+    }
+}
