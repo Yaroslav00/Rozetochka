2f7eba0f6f90d6837d3f97975c7b7e0db3ad08b9
Yurii Horoshko <yuriy.horoshkolv@gmail.com>
2019-12-07 13:22:26 +0200

    Added login service

---
+++ Rozetochka/Business/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Interfaces;
+using DataAccess.Dto;
+
+namespace Business.Services
+{
+    public class UserService: IUserService
+    {
+        public async Task<UserDto> Login(string username, string password)
+        {
+
+        }
+        public async Task<UserDto> Register(string username, string password)
+        {
+
+        }
+    }
+}
