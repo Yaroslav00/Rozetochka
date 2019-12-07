5981bb3622bb8ce421bd425d952fb5ecf2305319
SirStrash <sirstrash.xl@gmail.com>
2019-12-07 13:05:59 +0200

    Commited uncommited files, fixed previous versions. Added cart and order history for user; order history for admin. Made some improvements

--- Rozetochka/Rozetochka/MainWindow.xaml.cs
+++ Rozetochka/Rozetochka/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,6 @@ namespace Rozetochka
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool isUserAdmin = true;
         private ObservableCollection<Goods> goods { get; set; }
 
         public MainWindow()
@@ -21,12 +21,12 @@ namespace Rozetochka
 
             goodsList.ItemsSource = new ObservableCollection<Goods>
             {
-                new Goods(1,"iPhone X", 799, "expensive as shit"),
-                new Goods(2,"Macbook Pro 2019", 2199, "macbook"),
-                new Goods(3,"One plus 7 pro", 699, "oneplus"),
-                new Goods(4,"Rtx 2080ti", 1500, "rtx enabled"),
-                new Goods(5,"RX 580", 799, "simple radeon"),
-                new Goods(6,"hyperx Alloy Fps", 190, "descent keyboard")
+                new Goods(1,"iPhone X", 799, "expensive as shit"){ Category=new Category("Phones")},
+                new Goods(2,"Macbook Pro 2019", 2199, "macbook"){ Category=new Category("Laptops")},
+                new Goods(3,"One plus 7 pro", 699, "oneplus"){ Category=new Category("Phones")},
+                new Goods(4,"Rtx 2080ti", 1500, "rtx enabled"){ Category=new Category("GPUs")},
+                new Goods(5,"RX 580", 799, "simple radeon"){ Category=new Category("GPUs")},
+                new Goods(6,"hyperx Alloy Fps", 190, "descent keyboard"){ Category=new Category("Perifirals")},
             };
 
             categoriesList.ItemsSource = new ObservableCollection<Category>
@@ -36,17 +36,85 @@ namespace Rozetochka
                 new Category("Monitors"),
                 new Category("Perifirals"),
                 new Category("Laptops"),
+                new Category("Laptops"),
+                new Category("Laptops"),
             };
 
-            if (isUserAdmin)
+            ordersList.ItemsSource = new ObservableCollection<Order>
             {
-                OrderHistory.Visibility = Visibility.Collapsed;
-                Cart.Visibility = Visibility.Collapsed;
-            }
-            else
+                new Order{ID = 1,
+                    Data = DateTime.Now,
+                    TotalPrice = 1000,
+                    PaymentStatus = true,
+                    OrderedGood=new System.Collections.Generic.List<OrderedGood>(){
+                        new OrderedGood(){
+                            Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled"),
+                            Amount = 2,
+                            CurrentPrice = 1500
+                        },
+                        new OrderedGood(){
+                            Goods = new Goods(5,"RX 580", 799, "simple radeon"),
+                            Amount = 3,
+                            CurrentPrice = 799
+                        },
+                },
+            },
+                new Order{ID = 1,
+                    Data = DateTime.Now,
+                    TotalPrice = 1000,
+                    PaymentStatus = true,
+                    OrderedGood=new System.Collections.Generic.List<OrderedGood>(){
+                        new OrderedGood(){
+                            Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled"),
+                            Amount = 2,
+                            CurrentPrice = 1500
+                        },
+                        new OrderedGood(){
+                            Goods = new Goods(5,"RX 580", 799, "simple radeon"),
+                            Amount = 3,
+                            CurrentPrice = 799
+                        },
+                },
+            },
+                new Order{ID = 1,
+                    Data = DateTime.Now,
+                    TotalPrice = 1000,
+                    PaymentStatus = true,
+                    OrderedGood=new System.Collections.Generic.List<OrderedGood>(){
+                        new OrderedGood(){
+                            Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled"),
+                            Amount = 2,
+                            CurrentPrice = 1500
+                        },
+                        new OrderedGood(){
+                            Goods = new Goods(5,"RX 580", 799, "simple radeon"),
+                            Amount = 3,
+                            CurrentPrice = 799
+                        },
+                },
+            },
+        };
+
+            cartedGoods.ItemsSource = new ObservableCollection<CartedGoodDTO>
             {
-                Categories.Visibility = Visibility.Collapsed;
-            }
+                new CartedGoodDTO(){Amount = 2, Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled")},
+                new CartedGoodDTO(){Amount = 1, Goods = new Goods(5,"RX 580", 799, "simple radeon")},
+                new CartedGoodDTO(){Amount = 3, Goods =  new Goods(6,"hyperx Alloy Fps", 190, "descent keyboard")},
+                new CartedGoodDTO(){Amount = 2, Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled")},
+                new CartedGoodDTO(){Amount = 1, Goods = new Goods(5,"RX 580", 799, "simple radeon")},
+                new CartedGoodDTO(){Amount = 3, Goods =  new Goods(6,"hyperx Alloy Fps", 190, "descent keyboard")},
+                new CartedGoodDTO(){Amount = 2, Goods = new Goods(4,"Rtx 2080ti", 1500, "rtx enabled")},
+                new CartedGoodDTO(){Amount = 1, Goods = new Goods(5,"RX 580", 799, "simple radeon")},
+                new CartedGoodDTO(){Amount = 3, Goods =  new Goods(6,"hyperx Alloy Fps", 190, "descent keyboard")},
+            };
+
+            AddCategory.Visibility = Visibility.Collapsed;
+            AddGood.Visibility = Visibility.Collapsed;
+            Categories.Visibility = Visibility.Collapsed;
+            PersonalInfo.Visibility = Visibility.Collapsed;
+            Cart.Visibility = Visibility.Collapsed;
+            OrderHistory.Visibility = Visibility.Collapsed;
+
             this.DataContext = SessionData.Username;
             SessionData.UsernameChangedEvent += this.HandleUsernameChanged;
         }
@@ -56,16 +124,46 @@ namespace Rozetochka
             if (username != null)
             {
                 Greeting.Content = $"Вітаємо, {username}";
+                Username.Text = username;
 
                 LoginLabel.Content = "Вийти";
                 LoginLabel.Foreground = Brushes.Red;
+
+                PersonalInfo.Visibility = Visibility.Visible;
+                Cart.Visibility = Visibility.Visible;
+                OrderHistory.Visibility = Visibility.Visible;
+
+                if (username == "admin")
+                {
+                    OrderHistory.Visibility = Visibility.Collapsed;
+                    Cart.Visibility = Visibility.Collapsed;
+
+                    AddCategory.Visibility = Visibility.Visible;
+                    AddGood.Visibility = Visibility.Visible;
+                    Categories.Visibility = Visibility.Visible;
+                }
+               
                 return;
             }
 
-                Greeting.Content = null;
+            Goods.IsSelected = true;
 
-                LoginLabel.Content = "Увійти";
-                LoginLabel.Foreground = new SolidColorBrush(Color.FromRgb(76, 175, 80));
+            OrderHistory.Visibility = Visibility.Visible;
+            Cart.Visibility = Visibility.Visible;
+
+            AddCategory.Visibility = Visibility.Collapsed;
+            AddGood.Visibility = Visibility.Collapsed;
+            Categories.Visibility = Visibility.Collapsed;
+            PersonalInfo.Visibility = Visibility.Collapsed;
+            Cart.Visibility = Visibility.Collapsed;
+            OrderHistory.Visibility = Visibility.Collapsed;
+
+
+            Username.Text = null;
+            Greeting.Content = null;
+
+            LoginLabel.Content = "Увійти";
+            LoginLabel.Foreground = new SolidColorBrush(Color.FromRgb(76, 175, 80));
         }
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -74,7 +172,7 @@ namespace Rozetochka
 
         private void LoginLabel_MouseEnter(object sender, MouseEventArgs e)
         {
-            Label loginLabel = (Label) sender;
+            Label loginLabel = (Label)sender;
             loginLabel.FontWeight = FontWeights.SemiBold;
         }
 
@@ -98,12 +196,26 @@ namespace Rozetochka
             }
         }
 
+        private void AddGoodButton_Click(object sender, RoutedEventArgs e)
+        {
+            GoodWindow goodWindow = new GoodWindow();
+            goodWindow.ShowDialog();
+        }
+        private void AddCategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryWindow categoryWindow = new CategoryWindow();
+            categoryWindow.ShowDialog();
+        }
         private void goodsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
-
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
